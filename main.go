bee7b1aebeed2cd116d95f34ddfc0481ed46ad41
parkjinhong03 <jinhong0719@naver.com>
2019-11-27 20:27:13 +0900

    Context 구조체를 받아서 처리하기

--- main.go
+++ main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"fmt"
 	"github.com/parkjinhong03/http_practice/net"
-	"net/http"
 )
 
-func CustomMiddleware(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(r.Method, "요청이 들어왔습니다!")
+func CustomMiddleware(next net.HandlerFunc) net.HandlerFunc {
+	return func(c *net.Context) {
+		fmt.Println(c.Request.Method, "요청이 들어왔습니다!")
 
-		next(w, r)
+		next(c)
 		return
 	}
 }
@@ -18,17 +17,17 @@ func CustomMiddleware(next http.HandlerFunc) http.HandlerFunc {
 func main() {
 	s := net.NewServer()
 
-	s.HandleFunc("GET", "/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "asdf")
+	s.HandleFunc("GET", "/:user_id", func(c *net.Context) {
+		fmt.Fprintln(c.ResponseWriter, "asdf", c.Params["user_id"])
 	})
-	s.HandleFunc("GET", "/1", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "asdf")
+	s.HandleFunc("GET", "/1", func(c *net.Context) {
+		fmt.Fprintln(c.ResponseWriter, "asdf")
 	})
-	s.HandleFunc("GET", "/12", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "asdf")
+	s.HandleFunc("GET", "/12", func(c *net.Context) {
+		fmt.Fprintln(c.ResponseWriter, "asdf")
 	})
-	s.HandleFunc("GET", "/123", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "asdf")
+	s.HandleFunc("GET", "/123", func(c *net.Context) {
+		fmt.Fprintln(c.ResponseWriter, "asdf")
 	})
 
 	s.Use(CustomMiddleware)
