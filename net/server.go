a9bf160533aca9e3486b953ee3d3659a60471917
parkjinhong03 <jinhong0719@naver.com>
2019-11-27 13:14:04 +0900

    체인식으로 미들웨어를 묶고 서버 구동

---
+++ net/server.go
@@ -0,0 +1,36 @@
+package net
+
+import "net/http"
+
+type server struct {
+	*router
+	middlewares []middleware
+	startHandler http.HandlerFunc
+}
+
+func NewServer() *server {
+	r := &router{map[string]map[string]http.HandlerFunc{}}
+	s := &server{router:r}
+	s.middlewares = []middleware{
+		// ... 미들 웨어 추가
+		LogHandler,
+		RecoverHandler,
+	}
+	return s
+}
+
+func (s *server) Run(addr string) {
+	s.startHandler = s.router.handler()
+
+	for i := len(s.middlewares)-1; i>=0; i-- {
+		s.startHandler = s.middlewares[i](s.startHandler)
+	}
+
+	if err := http.ListenAndServe(addr, s); err != nil {
+		panic(err)
+	}
+}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	s.startHandler(w, req)
+}
